e0760e4f9db946012705db026899c0675ad2a2d1
German Gorodilov <45728269+gorodilovg@users.noreply.github.com>
2021-04-16 23:16:03 +0500

    Update settings.py

--- InvProd2/settings.py
+++ InvProd2/settings.py
@@ -62,8 +62,14 @@ WSGI_APPLICATION = 'InvProd2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'InvDB2',
+        'USER': 'postgres',
+        'PASSWORD': 'masterkey',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+        'OPTIONS': {
+        },
     }
 }
 
