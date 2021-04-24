90d08a0eea3375db3134858a0c02f294a0c7d4d6
Ildar <74951617+UrazbakhtinIVE@users.noreply.github.com>
2021-04-24 11:56:46 +0300

    Внесены корректировки в заголовки

--- outputs/forms.py
+++ outputs/forms.py
@@ -3,6 +3,7 @@ from django import forms
 from mainapp.models import Category
 from outputs.models import Monitor, Headset
 
+
 class OutputsCategoriesForm(forms.Form):
     categories = forms.ModelChoiceField(
         queryset=Category.objects.all(),
@@ -13,25 +14,25 @@ class OutputsCategoriesForm(forms.Form):
 class MonitorCreateForm(forms.ModelForm):
     class Meta:
         model = Monitor
-        fields = ('model', 'serialNumber')
+        fields = ('model', 'serialNumber', 'status')
 
         widgets = {
             'serialNumber': forms.TextInput(attrs={'class': 'form-control'}),
             'model': forms.Select(attrs={'class': 'form-control'}),
-            # 'status': forms.Select(attrs={'class': 'form-control'}),
+            'status': forms.Select(attrs={'class': 'form-control'}),
         }
 
 
 class MonitorUpdateForm(forms.ModelForm):
     class Meta:
         model = Monitor
-        fields = ( 'status','person','number', 'location','description')
+        fields = ('status', 'person', 'number', 'location', 'description')
 
         widgets = {
             'status': forms.Select(attrs={'class': 'form-control'}),
             'number': forms.TextInput(attrs={'class': 'form-control'}),
             'location': forms.Select(attrs={'class': 'form-control'}),
-            'description':forms.Textarea(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
 
             'person': autocomplete.ModelSelect2(
                 url="search-first-name-autocomplete",
